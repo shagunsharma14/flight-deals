f91b0918978e154f9379f46e172c352cfb246a40
GeekyNerd <85250765+BloodShoT14@users.noreply.github.com>
2022-12-23 00:21:12 +0530

    final commit
    All the critical information is not there in the code.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
-
 ORIGIN_CITY_IATA = "LON"
 
 data_manager = DataManager()
@@ -35,16 +34,23 @@ for destination_code in destinations:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+    print(flight.price)
     if flight is None:
         continue
 
     if flight.price < destinations[destination_code]["price"]:
+
+        users = data_manager.get_customer_emails()
+        emails = [row["email"] for row in users]
+        names = [row["firstName"] for row in users]
+
         message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
 
-        ######################
         if flight.stop_overs > 0:
             message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
-            print(message)
-        #######################
 
-        notification_manager.send_sms(message)
+        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+
+        notification_manager.send_emails(emails, message, link)
+
+
